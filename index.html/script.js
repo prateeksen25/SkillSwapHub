18e7b7cebfca4117c0a641029ce4eff3606ff2c9
Prateek Sen <prateeksen9928@gmail.com>
2025-08-16 17:11:15 +0530

    First commit

---
+++ index.html/script.js
@@ -0,0 +1,9 @@
+// For now, let's just test if JS is working
+console.log("SkillSwap Hub is ready!");
+// Get button from HTML
+let startButton = document.querySelector("button");
+
+// Add click event
+startButton.addEventListener("click", function () {
+    alert("Welcome to SkillSwap Hub! Let's find your skill match.");
+});
